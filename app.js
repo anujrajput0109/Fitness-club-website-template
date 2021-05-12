cfc126dbb34679559110d24b5cd0958ae5166518
Anuj <anujrajput199910@gmail.com>
2021-05-12 22:27:25 +0530

    Initialized project as node app, coverted html file to pug and created contact page and jion us page

---
+++ app.js
@@ -0,0 +1,31 @@
+const http=require("http");
+const fs=require("fs");
+const express=require("express");
+const path=require('path');
+
+const port=80;
+const app=express();
+app.use('/static', express.static('static'));
+
+app.set('view engine','pug');
+app.set('views',path.join(__dirname,'views'));
+
+app.get('/',(req,res)=>{
+    res.status(200).render('index.pug');
+})
+app.get('/contact',(req,res)=>{
+    let object={'title':"Fitness Club Contact Page"}
+    res.status(200).render('contact.pug',object);
+})
+app.get('/jion',(req,res)=>{
+    let object={'title':"Jion Our Fitness Club"}
+    res.status(200).render('jion.pug',object);
+})
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`application is running on port ${port}`)
+})
