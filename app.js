5b85d3f4f2aa74bd80a7ee05c0cd67b1146174ed
Anuj <anujrajput199910@gmail.com>
2021-05-14 13:39:06 +0530

    fixed some bugs and added file system to store contact details

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const fs=require("fs");
 const express=require("express");
 const path=require('path');
 
+
+
+
 const port=80;
 const app=express();
 app.use('/static', express.static('static'));
@@ -14,13 +17,21 @@ app.get('/',(req,res)=>{
     res.status(200).render('index.pug');
 })
 app.get('/contact',(req,res)=>{
-    let object={'title':"Fitness Club Contact Page"}
+    let object={'title':"Fitness Club Contact Page"};
     res.status(200).render('contact.pug',object);
 })
 app.get('/jion',(req,res)=>{
-    let object={'title':"Jion Our Fitness Club"}
+    let object={'title':"Jion Our Fitness Club"};
     res.status(200).render('jion.pug',object);
 })
+//post request for contact form details
+app.get('/submit',function(req,res){
+    let myData= `User name is ${req.query.name}. His phone number is ${req.query.phone}. email is ${req.query.email}. address is ${req.query.address}. His quetion is ${req.query.message}\n\n`;
+    // console.log(myData);
+    fs.appendFileSync("contact.txt", myData);
+    let object={'title':"Fitness Club Contact Page"};
+    res.status(200).render('contact.pug',object);
+});
 
 
 
